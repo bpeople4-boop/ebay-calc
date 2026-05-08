890ae04dad66dd7f17339ad71c0e4d1cd7189d2c
bpeople4-boop <b.people4@gmail.com>
2026-05-08 21:18:44 +0900

    Update ebay-api.js

--- api/ebay-api.js
+++ api/ebay-api.js
@@ -1,1 +1,15 @@
-
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).end();
+  
+  const { url, accessToken } = req.body;
+  
+  const response = await fetch(url, {
+    headers: {
+      "Authorization": `Bearer ${accessToken}`,
+      "X-EBAY-C-MARKETPLACE-ID": "EBAY_US",
+    },
+  });
+  
+  const data = await response.json();
+  res.status(response.status).json(data);
+}
